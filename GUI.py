451272c28e8b9b97846f7a129d53629a92cd6c25
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-02 16:57:48 +0200

    gui structure done

--- GUI.py
+++ GUI.py
@@ -10,6 +10,7 @@ from PyQt5.QtGui import QIcon
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
+
 class GUI(QDialog):
     def __init__(self):
         super(GUI, self).__init__()
@@ -20,14 +21,17 @@ class GUI(QDialog):
 
     def Objets(self):
         self.figSpec = Figure(figsize=(8, 4), dpi=100)
-        self.canvasSpec = FigureCanvas(self.figSpec)
-        # self.toolbarSpec = NavigationToolbar(self.canvasSpec, self)
         self.figTemp = Figure(figsize=(8, 4), dpi=100)
-        self.canvasTemp = FigureCanvas(self.figTemp)
         self.figMicro = Figure(figsize=(8, 4), dpi=100)
-        self.canvasMicro = FigureCanvas(self.figMicro)
         self.figSign = Figure(figsize=(15, 3), dpi=100)
+
+        self.canvasSpec = FigureCanvas(self.figSpec)
+        self.canvasTemp = FigureCanvas(self.figTemp)
+        self.canvasMicro = FigureCanvas(self.figMicro)
         self.canvasSign = FigureCanvas(self.figSign)
+
+        # self.toolbarSpec = NavigationToolbar(self.canvasSpec, self)
+
 
         self.Slider = QSlider(Qt.Horizontal)
 
@@ -44,11 +48,8 @@ class GUI(QDialog):
         MainLayout.addWidget(self.Slider)
         self.setLayout(MainLayout)
 
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     clock = GUI()
     clock.show()
     sys.exit(app.exec_())
-
