ce166fea78686b62888225de1c858ab72d43c4d8
OupsMajDsl <oupsmajdsl@gmail.com>
2019-05-02 16:38:12 +0200

    debut interface

--- GUI.py
+++ GUI.py
@@ -1,4 +1,54 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt 
-import PyQt5.Widgets
+from matplotlib.figure import Figure
+from PyQt5.QtWidgets import (QDialog, QApplication, QPushButton, QLabel, QCheckBox, QComboBox, 
+                            QVBoxLayout, QHBoxLayout, QGridLayout, QFileDialog, QMessageBox,
+                            QSpacerItem, QFrame, QSlider)
+from PyQt5.QtCore import Qt 
+from PyQt5.QtGui import QIcon
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+
+class GUI(QDialog):
+    def __init__(self):
+        super(GUI, self).__init__()
+        self.setWindowTitle("Ploc")
+
+        self.Objets()
+        self.display()
+        
+
+
+    def Objets(self):
+        self.figSpec = Figure(figsize=(8, 4), dpi=100)
+        self.canvasSpec = FigureCanvas(self.figSpec)
+        # self.toolbarSpec = NavigationToolbar(self.canvasSpec, self)
+        self.figTemp = Figure(figsize=(8, 4), dpi=100)
+        self.canvasSpec = FigureCanvas(self.figTemp)
+
+        self.figMicro = Figure(figsize=(8, 4), dpi=100)
+        self.figSign = Figure(figsize=(15, 3), dpi=100)
+        
+
+        self.Slider = QSlider(Qt.Horizontal)
+
+    def display(self):
+        MainLayout = QVBoxLayout()
+        grid = QGridLayout()
+        # grid.addWidget(self.video, 0, 0)
+        grid.addWidget(self.figspec, 1, 0)
+        grid.addWidget(self.figtemp, 0, 1)
+        grid.addWidget(self.figmicro, 1, 1)
+
+        MainLayout.addLayout(grid)
+        MainLayout.addWidget(self.figsign)
+        MainLayout.addWidget(self.Slider)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clock = GUI()
+    clock.show()
+    sys.exit(app.exec_())
 
